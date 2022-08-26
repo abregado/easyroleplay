6ea5be189961fc85931c45bc1caae4f8d5c068e9
dostalma <dostalma@gmail.com>
2022-08-26 07:41:45 +0200

    NPC attributes (#13)
    * NPC attributes

--- sections/npc/npcgenerator.js
+++ sections/npc/npcgenerator.js
@@ -1,5 +1,7 @@
 import {NpcNames} from "./npc-data.js";
 import {NpcProfessions} from "./npc-data.js";
+import {NpcEmotions} from "./npc-data.js";
+import {NpcBonds} from "./npc-data.js";
 
 class NpcGenerator {
 
@@ -14,6 +16,15 @@ class NpcGenerator {
         let profession = NpcProfessions[randomIndex];
         this.#replaceText("npc-profession", profession);
 
+        randomIndex = Math.floor(Math.random() * 100) % Object.keys(NpcEmotions).length;
+        let emotionCategory = Object.values(NpcEmotions)[randomIndex];
+        randomIndex = Math.floor(Math.random() * 1000) % emotionCategory.length;
+        let emotion = emotionCategory[randomIndex];
+        this.#replaceText("npc-quirk", emotion);
+
+        randomIndex = Math.floor(Math.random() * 1000) % NpcBonds.length;
+        let bond = NpcBonds[randomIndex];
+        this.#replaceText("npc-bond", bond);
     }
 
     #replaceText(id, text) {
